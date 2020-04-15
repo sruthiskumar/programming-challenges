0dd3fc890446d586ea682c4d3fcc15528bce9858
Sruthi Sree Kumar <sruthiskumar92@gmail.com>
2020-04-15 20:33:19 +0200

    Create MaximumAbsDiff.java

---
+++ MaximumAbsDiff.java
@@ -0,0 +1,21 @@
+/**
+ * Given an integer array of N integers, Return maximum value of f(i, j) f
+ * or all 1 ≤ i, j ≤ N. f(i, j) is defined as |A[i] - A[j]| + |i - j|, 
+ * where |x| denotes absolute value of x.
+ */
+ 
+public class MaximumAbsDiff {
+      public int maxArr(ArrayList<Integer> A) {
+     int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int i = 0; i < A.size(); i++) {
+            max1 = Math.max(max1, A.get(i) + i);
+            min1 = Math.min(min1, A.get(i) + i);
+            max2 = Math.max(max2, A.get(i) - i);
+            min2 = Math.min(min2, A.get(i) - i);
+        }
+        return Math.max(max1-min1, max2-min2);
+    }
+}
